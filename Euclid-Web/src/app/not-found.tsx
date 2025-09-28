eb917820218302b600c8988ee4b841bf5c21d1d6
Niyaz Nassyrov <nassyrovich@gmail.com>
2025-09-27 23:30:14 -0400

    [EUCLID-30] Add ui kit and layout shell
    Header sticky, footer visible, 404 present.

---
+++ Euclid-Web/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center" style={{ height: "calc(100vh - 106px - 328px - 96px)" }}>
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-2xl">PAGE NOT FOUND</p>
+    </div>
+  );
+};
---
+++ Euclid-Web/src/components/ui/shellhacks.tsx
@@ -0,0 +1,7 @@
+export default function ShellHacks() {
+  return (
+    <p className="font-bold italic">
+      SHELLHACKS 2025
+    </p>
+  )
+}
